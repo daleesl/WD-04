78c8f54b76827218c122f573e4d1ce8840e19778
daleesl <daleandrewslee@gmail.com>
2025-11-25 20:19:10 +0800

    Populated the infos using JSON

---
+++ info.js
@@ -0,0 +1,81 @@
+var members = [
+    {
+        "name": "Monkey D. Luffy",
+        "picture": "assets/images/characters/sh_luffy.jpg"
+    },
+    {
+        "name": "Roronoa Zoro",
+        "picture": "assets/images/characters/sh_zoro.jpg"
+    },
+    {
+        "name": "Sanji",
+        "picture": "assets/images/characters/sh_sanji.jpg"
+    },
+    {
+        "name": "Jinbe",
+        "picture": "assets/images/characters/sh_jinbei.jpg"
+    },
+    {
+        "name": "Nico Robin",
+        "picture": "assets/images/characters/sh_robin.jpg"
+    },
+    {
+        "name": "Usopp",
+        "picture": "assets/images/characters/sh_ussop.jpg"
+    },
+    {
+        "name": "Franky",
+        "picture": "assets/images/characters/sh_franky.jpg"
+    },
+    {
+        "name": "Brook",
+        "picture": "assets/images/characters/sh_brook.jpg"
+    },
+    {
+        "name": "Nami",
+        "picture": "assets/images/characters/sh_nami.jpg"
+    },
+    {
+        "name": "Tony Tony Chopper",
+        "picture": "assets/images/characters/sh_chopper.jpg"
+    }
+];
+
+var fruits =[
+    {
+        "name": "Gomu Gomu no Mi",
+        "ability": "Rubber Body",
+        "picture": "assets/images/fruits/dv_luffy.png",  
+        "owner": "Monkey D. Luffy"
+    },
+    {
+        "name": "Hito Hito no Mi",
+        "ability": "Human Transformation",
+        "picture": "assets/images/fruits/dv_chopper.png",  
+        "owner": "Tony Tony Chopper"
+    },
+    {
+        "name": "Hana Hana no Mi",
+        "ability": "Body Part Replication",
+        "picture": "assets/images/fruits/dv_robin.png",  
+        "owner": "Nico Robin"
+    },
+    {
+        "name": "Yomi Yomi no Mi",
+        "ability": "Revival and Soul Manipulation",
+        "picture": "assets/images/fruits/dv_brook.png",  
+        "owner": "Brook"
+    }
+
+];
+
+var Ships = [
+    {
+        "name": "Going Merry",
+        "picture": "assets/images/ships/ship_merry.png"
+    },
+    {
+        "name": "Thousand Sunny",
+        "picture": "assets/images/ships/ship_sunny.png"
+    }
+];
